57616549cd9b896b52e754d4fb8120857af35b4a
Kaleb <kalebzambrano@outlook.com>
2025-01-30 17:05:10 -0500

    finishing excercise

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -6,48 +6,56 @@ const App = () => {
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
-  const [newName, setNewName] = useState('');
-  const [newNumber, setNewNumber] = useState('');
-  const [filter, setFilter] = useState(''); 
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('') 
+  const [error, setError] = useState(false)
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value);
-  };
+    setNewName(event.target.value)
+  }
 
   const handleNumberChange = (event) => {
-    setNewNumber(event.target.value);
-  };
+    setNewNumber(event.target.value)
+  }
 
   const handleFilterChange = (event) => {
-    setFilter(event.target.value.toLowerCase());  
-  };
+    setFilter(event.target.value.toLowerCase())
+  }
 
   const addPerson = (event) => {
-    event.preventDefault();
+    event.preventDefault()
+    const phoneReq = /^[0-9-]+$/
+      
     if (newName.trim() === '' || newNumber.trim() === '') {
-      alert('Completa todos los campos');
+      alert('Completa todos los campos')
+    } else if (!phoneReq.test(newNumber)) {
+      setError(true)
     } else if (!persons.some(person => person.name === newName || person.number === newNumber)) {
-      setPersons([...persons, { name: newName, number: newNumber, id: persons.length + 1 }]);
-      setNewName('');
-      setNewNumber('');
+      setError(false)
+      setPersons([...persons, { name: newName, number: newNumber, id: persons.length + 1 }])
+      setNewName('')
+      setNewNumber('')
     } else {
-      alert(`${newName} or ${newNumber} is already added to phonebook`);
-      setNewName('');
-      setNewNumber('');
+      setError(false)
+      alert(`${newName} or ${newNumber} is already added to phonebook`)
+      setNewName('')
+      setNewNumber('')
     }
-  };
+  }
+  
 
   // Filter
   const filteredPersons = persons.filter(person =>
     person.name.toLowerCase().includes(filter)
-  );
+  )
 
   return (
     <div>
       <h2>Phonebook</h2>
       <div>
-        filter shown with <input type="text" value={filter} onChange={handleFilterChange} />
+        filter shown with <input name='filter' type="text" value={filter} onChange={handleFilterChange} />
       </div>
       <h2>Add a new</h2>
       <form onSubmit={addPerson}>
@@ -55,6 +63,7 @@ const App = () => {
           name: <input name='name' value={newName} onChange={handleNameChange} />
         </div>
         <div>number: <input name='number' value={newNumber} onChange={handleNumberChange} /></div>
+        {error && <h4 style={{color: 'red'}}>You can only type numbers or hyphens</h4>}
         <div>
           <button type="submit">add</button>
         </div>
@@ -64,7 +73,7 @@ const App = () => {
         <p key={person.id}>{person.name} {person.number}</p>
       ))}
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
